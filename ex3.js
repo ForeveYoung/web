d9201075af11dd8f0bf233ff0cd7f259ed0fd018
ForeveYoung <hrrraon@gmail.com>
2020-05-15 17:12:46 +0300

    eagdc

--- ex3.js
+++ ex3.js
@@ -1,7 +1,7 @@
-let btn = document.getElementById('bee');
+let btn = document.getElementById('btn');
 btn.addEventListener('click', paragraphClick);
 
 function paragraphClick() {
-  alert('Ви натиснули на параграф!');
+  alert('Ви натиснули на кнопку!');
 }
 
